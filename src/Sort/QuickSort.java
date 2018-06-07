cf06de38bcef932436bb77bd9fcaafae36e8487e
geonyoungkim <c2619zz@naver.com>
2018-06-07 14:40:33 +0900

    알고리즘 - 퀵소트 1. 평균적인 시간 복잡도 O(nlogn), 최악의 시간복잡도 O(n^2) 2. 피벗아이템을 정하여 피벗 아이템의 왼쪽은 작게 오른쪽은 크게라는 조건에 부응하도록 자리 배치. 3. 각 피벗아이템으로 나누어진 그룹에 대해서 똑같이 수행하도록하여 정렬을 시킨다.

---
+++ src/Sort/QuickSort.java
@@ -0,0 +1,46 @@
+package Sort;
+
+public class QuickSort {
+	
+	public static void main(String[] args) {
+		int[] arrs = { 69, 10, 30, 2, 16, 8, 31, 22 };
+
+		quickSort(arrs, 0, arrs.length - 1);
+		System.out.println("결과");
+
+		for (int i : arrs) {
+			System.out.print(i + " ");
+		}
+	}
+	
+	
+	public static void quickSort(int[] arrs,int left,int right) {
+		
+		if(left<right) {
+			int pivot=partition(arrs,left,right);
+			
+			quickSort(arrs, left, pivot-1);
+			quickSort(arrs, pivot+1, right);
+		}
+	}
+	
+	public static int partition(int[] arrs,int left,int right) {
+		int mid=arrs[((left+right)/2)];
+		while(left<right) {
+			while(arrs[left]<mid) {
+				left++;
+			}
+			while(arrs[right]>mid) {
+				right--;
+			}
+			int temp=arrs[left];
+			arrs[left]=arrs[right];
+			arrs[right]=temp;
+		}
+		
+		
+		
+		return left;
+	}
+
+}
