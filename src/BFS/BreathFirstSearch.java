4a1280d55ba179c3c17c583cdd13f4549f8ac24e
geonyoungkim <c2619zz@naver.com>
2018-06-07 10:29:31 +0900

    알고리즘 너비우선탐색 큐를 사용하여 문제 풀이. 먼저 그래프를나타내기 위한 인접행렬을 생성. 그 후, 1. 큐에 첫 노드삽입, 2. 방문 체크로 변경 한 뒤 재귀를 돌린다. 재귀에서는 queue의 값을 빼어 해당 노드의 인접해있으며 방문하지 않은것을 다시 큐에 넣고 방문 체크로 변경하는 작업을 반복한다.

---
+++ src/BFS/BreathFirstSearch.java
@@ -0,0 +1,49 @@
+package BFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BreathFirstSearch {
+	
+	public static void main(String[] args) {
+		int[][] array= {
+				{0,1,1,0},
+				{1,0,0,1},
+				{1,0,0,1},
+				{0,1,1,0}
+		};
+		
+		int[] visited=new int[array.length];
+		
+		visited[0]=1;
+		
+		Queue<Integer> queue=new LinkedList<>();
+		queue.add(0);
+		
+		bfs(array,visited,queue,"1");
+	}
+	public static void bfs(int[][] array,int[] visited,Queue<Integer> queue,String path) {
+		int size=queue.size();
+		
+		while(size-->0) {
+			int location=queue.poll();
+			for(int i=0;i<array[location].length;i++) {
+				if(visited[i]==0&&array[location][i]==1) {
+					queue.add(i);
+					path+=String.valueOf(i+1);
+					visited[i]=1;
+				}
+			}
+		}
+		
+		if(queue.size()!=0) {
+			bfs(array,visited,queue,path);
+		}else {
+			System.out.println(path);
+		}
+		
+		
+		
+	}
+
+}
